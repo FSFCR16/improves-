6372dbc653df36e8bd86cf12b6d80d882b2d04d8
FSFCR16 <Fajarfsabo161@gmail.com>
2023-10-07 01:24:04 -0500

    post

---
+++ script.js
@@ -0,0 +1,17 @@
+function activar() {
+    var updateButton = document.getElementById("updateDetails");
+    var cancelButton = document.getElementById("cancel");
+    var favDialog = document.querySelector(".ventanaPost");
+
+    // Update button opens a modal dialog
+    updateButton.addEventListener("click", function () {
+      favDialog.showModal();
+    });
+
+    // Form cancel button closes the dialog box
+    cancelButton.addEventListener("click", function () {
+      favDialog.close();
+    });
+}
+
+activar()
