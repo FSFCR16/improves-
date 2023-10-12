6eb2da93aa0a1fd2959865c16fe254962824af8f
FSFCR16 <Fajarfsabo161@gmail.com>
2023-10-12 16:07:23 -0500

    nuevo

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
+let botones=document.querySelectorAll(".btnPost")
+let inputTextTarea=document.querySelector(".inputTitle")
+let numInput=document.querySelector(".numInput")
+
 function activar() {
-    var updateButton = document.getElementById("updateDetails");
-    var cancelButton = document.getElementById("cancel");
-    var favDialog = document.querySelector(".ventanaPost");
+    let updateButton = document.getElementById("updateDetails");
+    let cancelButton = document.getElementById("cancel");
+    let favDialog = document.querySelector(".ventanaPost");
 
     // Update button opens a modal dialog
     updateButton.addEventListener("click", function () {
       favDialog.showModal();
     });
 
-    // Form cancel button closes the dialog box
-    cancelButton.addEventListener("click", function () {
-      favDialog.close();
-    });
 }
 
 activar()
+
+/*Funciones botones*/
+console.log(botones)
+function hover(){
+
+    for (let i=0; i<botones.length; i++){
+
+        botones[i].addEventListener("click", function(e){
+
+            let va= e.target
+            resetbtn()
+            va.classList.toggle("btnPostDos")
+        })
+    }
+}
+
+hover()
+
+function resetbtn() {
+
+    for (let i=0 ; i<botones.length;i++){
+
+    botones[i].classList.remove("btnPostDos")
+
+    }
+}
+
+/*funciones input TextTarea*/
+
+
+function contadorLetras(){
+    let contador=0
+
+    inputTextTarea.addEventListener("keydown", function(e){
+
+        if(contador!=300){
+            numInput.innerHTML=numInput.innerHTML.replace(contador,contador+1)
+            contador=contador+1
+        }
+    })
+}
+contadorLetras()
+
