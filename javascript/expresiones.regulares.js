1cad9696db2f02afbb9ea6893cd73ba69149c397
FSFCR16 <fajarfsabo161@gmail.com>
2023-12-07 23:15:21 -0500

    update

---
+++ javascript/expresiones.regulares.js
@@ -0,0 +1,5 @@
+// Las expresiones regulares son patrones usados parar hacer match con combinaciondes de caracteres 
+// en formato String, en javaScript una expresion regular es un objeto.
+
+// Estos patrones son usados con los metodos exce() y test() del objeto RegExp, and con los metdos
+// match, matchAll, replace, replaceAll y split
--- spotify/index.js
+++ spotify/index.js
@@ -2,41 +2,48 @@ const barra2 = document.querySelector(".barra-2")
 const flechaDerecha = document.querySelector(".flecha-derecha")
 const flechaIzquierda = document.querySelector(".flecha-izquierda")
 
-const step = 50
+const step = 60
+//ScrollWidth son la cantidad de pixeles que tiene un contenedor internamente
+//ClientWidth sirve para saber la cantidad de pixeles visibles en un contendor
+//scrollLeft la cantidad de pixeles que se movio hacia cierto lado
 
-function ajustarFlechas(){
-    if(barra2.scrollWidth === barra2.clientWidth){
+function ajustarFlechas() {
+    if (barra2.scrollWidth === barra2.clientWidth) {
         flechaDerecha.setAttribute("hidden", true)
         flechaIzquierda.setAttribute("hidden", true)
-    }else{
+    } else {
         flechaDerecha.removeAttribute("hidden")
     }
 }
-
-
-window.onresize = function(){
+window.onresize = function () {
     ajustarFlechas()
 }
-window.onload= function()
-{
+window.onload = function () {
     ajustarFlechas()
 }
-flechaDerecha.addEventListener("click", (e) => {
+
+flechaDerecha.addEventListener("click", () => {
+    // let can = barra2.scrollLeft
     barra2.scrollLeft += step
+    // let scrollLeftt = barra2.scrollLeft
+
     flechaIzquierda.removeAttribute("hidden")
-    if(barra2.scrollLeft === (barra2.scrollWidth - barra2.clientWidth )){
+    if (barra2.scrollLeft === (barra2.scrollWidth - barra2.clientWidth)) {
         flechaDerecha.setAttribute("hidden", true)
     }
-
 })
 
-flechaIzquierda.addEventListener("click", (e) => {
+flechaIzquierda.addEventListener("click", () => {
     barra2.scrollLeft -= step
-    if(barra2.scrollLeft !== 0){
+    if (barra2.scrollLeft !== 0) {
         flechaDerecha.removeAttribute("hidden")
-    }else{
+    }
+    if (barra2.scrollLeft === 0) {
         flechaIzquierda.setAttribute("hidden", true)
-        
+        flechaDerecha.removeAttribute("hidden")
 
     }
 })
+
+
+/*Opacar los laterales del contendor de las flechas*/
