2a154a5fbe33368ccde418e706d5d3cb2c541156
FSFCR16 <Fajarfsabo161@gmail.com>
2024-03-22 07:50:53 -0500

    update

---
+++ javascript/poo/poo_principios.js
@@ -0,0 +1,11 @@
+// El principal proposito de la progrmacion orientada a objetos es modularizar el codigo
+// y que el codigo sea reutilizable
+
+// Modularizar entiendase como la forma de separar un codigo en pequeñas partes para que sea mas entendible
+// ahora como se modulariza cada una de estas partes tiene un proposito y como tienen un proposito se puede reutilizar y asi no tenemos que escribir
+// el mismo codigo varias veces
+
+
+// Minimo hay dos requiriemientos para que algo sea orientado a objetos o no
+// El primero es la capacidad de modelar problemas atraves de objetos
+// El segundo es el soporte de algunos principios que ganarantice la modularidad y reutilizacion de codigo
